e3aaf22db9635a9b45d2d5d8bde6befdefab1db6
Joseph Dillon <joseph.dillon.522@gmail.com>
2021-12-19 13:40:47 -0500

    sass integration

---
+++ index.js
@@ -0,0 +1,20 @@
+import sass from "sass";
+import { promisify } from "util";
+import { writeFile } from "fs";
+const sassRenderPromise = promisify(sass.render);
+const writeFilePromise = promisify(writeFile);
+
+async function main() {
+  const styleResult = await sassRenderPromise({
+    file: `${process.cwd()}/styles.scss`,
+    outFile: `${process.cwd()}/styles.css`,
+    sourceMap: true,
+    sourceMapContents: true,
+    outputStyle: "compressed",
+  });
+
+  await writeFilePromise("styles.css", styleResult.css, "utf8");
+
+  await writeFilePromise("styles.css.map", styleResult.map, "utf8");
+}
+main();
